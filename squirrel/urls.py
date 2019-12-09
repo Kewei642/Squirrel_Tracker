190342b68030ee7e63fec19984c1b8d383d1393c
Kewei642 <kc3320@columbia.edu>
2019-12-08 23:01:21 -0500

    Add index page

--- squirrel/urls.py
+++ squirrel/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
+    path('', views.index),
     path('map/', include('map.urls')),
     path('sightings/',include('sightings.urls')),
     path('admin/', admin.site.urls)
